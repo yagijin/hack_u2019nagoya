883ca76aa050972e1ffb4bc057d9c50e830ed862
maitaken <150441004@ccmailg.meijo-u.ac.jp>
2019-08-08 13:07:30 +0900

    Update : コメントの追加

--- packet_capture/main.js
+++ packet_capture/main.js
@@ -1,27 +1,29 @@
 let pcap = require('pcap')
-let tcp_tracker = new pcap.TCPTracker()
 
+let tcp_tracker = new pcap.TCPTracker()
 let pcap_session = pcap.createSession('en0', "arp");
 
 // ここにターゲットにするデバイスのMACアドレス
 // const TARGET = 
 
+/**
+ * 配列の内容が同一であるかの判定を行う
+ * @param {[]int} arr 比較するMACアドレス
+ */
 function compareArray(arr) {
-  let flag = true
   for (var i = 0;i < TARGET.length;i++) {
     if (TARGET[i] != arr[i]) {
-      flag = false
+      return false
     }
   }
-
-  return flag
+  return true
 }
 
 pcap_session.on('packet', function (raw_packet) {
   let packet = pcap.decode.packet(raw_packet);
-  console.log(packet["payload"]["shost"])
   let macAddr = packet["payload"]["shost"]["addr"]
 
+  // TargetのMACアドレスと一致した時
   if (compareArray(macAddr)) {
     console.log("これは完全にやぎじん")
   }

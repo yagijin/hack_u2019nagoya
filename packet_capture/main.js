8ff3d3de231c3bce9b15f2f09a17aa4949c61593
maitaken <150441004@ccmailg.meijo-u.ac.jp>
2019-08-09 16:19:48 +0900

    Update : POST以外のリクエストが来た場合のエラー処理の追加

--- packet_capture/main.js
+++ packet_capture/main.js
@@ -3,11 +3,12 @@ let http = require('http')
 
 //MACアドレスを他ファイルからimport
 const MACADDR = require('./macAddr.js')
+const PORT = 8080
 
 
 let tcp_tracker = new pcap.TCPTracker()
-let pcap_session = pcap.createSession('wlan0', "arp")
-
+//let pcap_session = pcap.createSession('wlan0', "arp")
+let pcap_session = pcap.createSession('en0','arp')
 let server = http.createServer()
 
 // ここにターゲットにするデバイスのMACアドレス
@@ -36,20 +37,34 @@ pcap_session.on('packet', function (raw_packet) {
   }
 })
 
+console.log('HTTP Server Start')
+console.log("Listen ",PORT)
 server.on('request',setMacAddr)
-server.listen(8080)
+server.listen(PORT)
 
 // MacAddrの変更を行う
 function setMacAddr(req,res) {
   
+  // POST以外の場合　エラーメッセージを送信
   if(req.method != 'POST') {
+    errRsp = JSON.stringify({
+      "type" : "Method Error",
+      "message" : "Only post request"
+    })
+    res.write(errRsp)
     res.end()
     return
   }
 
+  // 監視対象のMACアドレスの変更
   req.on('data',(chunk) => {
     let reqData = JSON.parse(chunk)
     targetMacAddr = reqData['macAddr']
+    let successRsp = JSON.stringify({
+      "type" : "Success",
+      "message" : "Target MACAddress Changed!"
+    })
+    res.write(successRsp)
     res.end()
   })
 }
